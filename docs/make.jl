8c0d1341873f31efff8077a8b904641255c48bbc
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-11-11 01:12:18 +0530

    M  docs/make.jl M  docs/src/example.html

--- docs/make.jl
+++ docs/make.jl
@@ -14,19 +14,15 @@ PlutoSliderServer.export_notebook("examples/notebook.jl")
 mv("examples/notebook.html", "docs/src/example.html", force=true)
 
 sitename = "PlutoReport.jl"
-pages = [
-    "PlutoReport" => "index.md",
-    "Example Notebook" => "example.html"
-]
-
 # Using MathJax3 since Pluto uses that engine too.
+
 mathengine = MathJax3()
 prettyurls = get(ENV, "CI", nothing) == "true"
 format = HTML(; mathengine, prettyurls)
 modules = [PlutoSliderServer]
 strict = true
 checkdocs = :none
-makedocs(; sitename, pages, format, modules, strict, checkdocs)
+makedocs(; sitename, format, modules, strict, checkdocs)
 
 deploydocs(;
     repo = "github.com/DhruvaSambrani/PlutoReport.jl.git"
