21344955bc5c3a78925836ff0773ecdb62f9a967
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-11-10 23:18:20 +0530

    Update? docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,17 @@
 push!(LOAD_PATH, "../src/")
 
-using Documenter, PlutoReport
+using Documenter
+using PlutoReport
 
-makedocs(sitename = "PlutoReport.jl Documentation")
+makedocs(
+    sitename = "PlutoReport",
+    format = Documenter.HTML(),
+    modules = [PlutoReport]
+)
 
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
 deploydocs(
-    repo = "github.com/DhruvaSambrani/PlutoReport.jl.git",
+    repo = "github.com/DhruvaSambrani/PlutoReport.jl.git"
 )
